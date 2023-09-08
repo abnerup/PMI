093323df072e501d386b4b663ef6ba01c644af2e
aurbano <aurbano@WS77727>
2023-09-08 16:34:59 -0300

    Add project files.

---
+++ PMS.API/Data/ApplicationDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PMS.API.Models;
+
+namespace PMS.API.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext (DbContextOptions<ApplicationDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PMS.API.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ PMS.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace PMS.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
